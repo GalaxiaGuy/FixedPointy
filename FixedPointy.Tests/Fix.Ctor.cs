58be44d7aab90eea33999403ec7be8f726979e4e
Oliver Brown <oliver.brown@chelsea-apps.com>
2017-01-19 16:04:40 +0000

    Add constructor test.

---
+++ FixedPointy.Tests/Fix.Ctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FixedPointy.Tests
+{
+    [TestFixture]
+    public class FixCtorTests
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(int.MaxValue)]
+        [TestCase(int.MinValue)]
+        public void FixConstructorWorks(int raw)
+        {
+            var fix = new Fix(raw);
+
+            Assert.AreEqual(fix.Raw, raw, "Raw value is not correct");
+        }
+    }
+}
