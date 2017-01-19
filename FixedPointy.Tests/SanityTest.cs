8fbe09ec2b308b54fa81258bcefbff8b178cda5b
Oliver Brown <oliver.brown@chelsea-apps.com>
2017-01-19 15:54:43 +0000

    Add stub tests project.

---
+++ FixedPointy.Tests/SanityTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace FixedPointy.Tests
+{
+    [TestFixture]
+    public class SanityTest
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
